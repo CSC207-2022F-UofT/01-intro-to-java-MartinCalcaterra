ad1c274dc2fde6f091966e85d265194c4c8549b4
Martin Calcaterra <martin.calcaterra@mail.utoronto.ca>
2022-10-09 16:40:37 -0400

    Basics.java complete

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,7 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,6 +100,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -142,10 +146,20 @@ public class Basics {
          */
         StringBuilder ret = new StringBuilder();
 
-        // Fill in the rest of the body here
+        String[] words = to_split.split(" ");
+
+        ret.append(words[0].charAt(0));
+        ret.append(words[1].charAt(0));
+        ret.append(words[2].charAt(0));
+        ret.append(words[3].charAt(0));
+        ret.append(words[4].charAt(0));
+        ret.append(words[5].charAt(0));
+        ret.append(words[6].charAt(0));
 
         return ret.toString();
     }
+
+
 
     /**
      * 7. Below is a method that takes in an integer array (similar to a list
@@ -170,6 +184,12 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+
+        int arr_len = arr.length;
+
+        for (int i = 1; i <= arr_len; i += 2){
+            current_sum += arr[i];
+        }
 
         return current_sum;
     }
